773b656dd3f702cd90ac8795221f2bc624236f69
pchchv <ipchchv@gmail.com>
2025-06-26 21:15:32 +0300

    Add tests of validate Result type behavior

--- resultext/result_test.go
+++ resultext/result_test.go
@@ -1,8 +1,29 @@
 package resultext
 
-import "errors"
+import (
+	"errors"
+	"testing"
+
+	. "github.com/pchchv/go-assert"
+)
 
 type Struct struct{}
+
+func TestResult(t *testing.T) {
+	result := returnOk()
+	Equal(t, true, result.IsOk())
+	Equal(t, false, result.IsErr())
+	Equal(t, true, result.Err() == nil)
+	Equal(t, Struct{}, result.Unwrap())
+
+	result = returnErr()
+	Equal(t, false, result.IsOk())
+	Equal(t, true, result.IsErr())
+	Equal(t, false, result.Err() == nil)
+	PanicMatches(t, func() {
+		result.Unwrap()
+	}, "Result.Unwrap(): result is Err")
+}
 
 func returnOk() Result[Struct, error] {
 	return Ok[Struct, error](Struct{})
