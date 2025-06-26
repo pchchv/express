2f54a5c59dd62d93c979ea75197e88d7486d1f8d
pchchv <ipchchv@gmail.com>
2025-06-26 21:12:14 +0300

    Implement returnOk and returnErr functions for Result type testing

--- resultext/result_test.go
+++ resultext/result_test.go
@@ -1,3 +1,13 @@
 package resultext
 
+import "errors"
+
 type Struct struct{}
+
+func returnOk() Result[Struct, error] {
+	return Ok[Struct, error](Struct{})
+}
+
+func returnErr() Result[Struct, error] {
+	return Err[Struct, error](errors.New("bad"))
+}
