d531c3be3debe5b476ed0fbb00da5a6df52a1905
pchchv <ipchchv@gmail.com>
2025-06-26 20:56:28 +0300

    Implement Unwrap and UnwrapOr methods for Result type

--- resultext/result.go
+++ resultext/result.go
@@ -6,3 +6,22 @@ type Result[T, E any] struct {
 	err  E
 	isOk bool
 }
+
+// Unwrap returns the values of the result.
+// It panics if there is no result due to not checking for errors.
+func (r Result[T, E]) Unwrap() T {
+	if r.isOk {
+		return r.ok
+	}
+
+	panic("Result.Unwrap(): result is Err")
+}
+
+// UnwrapOr returns the contained Ok value or a provided default.
+func (r Result[T, E]) UnwrapOr(value T) T {
+	if r.isOk {
+		return r.ok
+	}
+
+	return value
+}
