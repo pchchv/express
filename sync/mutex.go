43357e5bb83db9786ce26c1b10b28d26c3cb02cf
pchchv <ipchchv@gmail.com>
2025-06-27 22:11:59 +0300

    Implement Lock and Unlock methods for Mutex type to manage safe access and mutation

--- sync/mutex.go
+++ sync/mutex.go
@@ -27,3 +27,25 @@ func NewMutex[T any](value T) Mutex[T] {
 		value: value,
 	}
 }
+
+// Lock locks the Mutex and returns value for use,
+// safe for mutation if the lock is already in use,
+// the calling goroutine blocks until the mutex is available.
+func (m Mutex[T]) Lock() MutexGuard[T, *sync.Mutex] {
+	m.m.Lock()
+	return MutexGuard[T, *sync.Mutex]{
+		m: m.m,
+		T: m.value,
+	}
+}
+
+// Unlock unlocks the Mutex accepting a value to set as the new or mutated value.
+// It is optional to pass a new value to be set but NOT required for there reasons:
+// 1. If the internal value is already mutable then no need to set as changes apply as they happen.
+// 2. If there's a failure working with the locked value you may NOT want to set it, but still unlock.
+// 3. Supports locked values that are not mutable.
+//
+// It is a run-time error if the Mutex is not locked on entry to Unlock.
+func (m Mutex[T]) Unlock() {
+	m.m.Unlock()
+}
