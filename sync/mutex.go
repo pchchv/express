888fb833d2e6de2cc8a3f1db462244df46998f4b
pchchv <ipchchv@gmail.com>
2025-06-27 22:04:21 +0300

    Add MutexGuard type for safe access to Mutex contents

--- sync/mutex.go
+++ sync/mutex.go
@@ -2,6 +2,12 @@ package syncext
 
 import "sync"
 
+// MutexGuard protects the inner contents of a Mutex for safety and unlocking.
+type MutexGuard[T any, M interface{ Unlock() }] struct {
+	m M
+	T T // is the inner generic type of the Mutex
+}
+
 // Mutex creates a type safe mutex wrapper ensuring one cannot access the
 // values of a locked values without first gaining a lock.
 type Mutex[T any] struct {
