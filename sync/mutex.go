6903e71c2950b3ab01537f1762dd1a57a4c9e676
pchchv <ipchchv@gmail.com>
2025-06-27 22:01:27 +0300

    Add type-safe Mutex wrapper to syncext package

--- sync/mutex.go
+++ sync/mutex.go
@@ -1,1 +1,10 @@
 package syncext
+
+import "sync"
+
+// Mutex creates a type safe mutex wrapper ensuring one cannot access the
+// values of a locked values without first gaining a lock.
+type Mutex[T any] struct {
+	m     *sync.Mutex
+	value T
+}
