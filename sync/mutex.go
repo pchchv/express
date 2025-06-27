ea26df463cee6f3167c3668466447b1d5686b58f
pchchv <ipchchv@gmail.com>
2025-06-27 22:24:47 +0300

    fix

--- sync/mutex.go
+++ sync/mutex.go
@@ -1,6 +1,10 @@
 package syncext
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/pchchv/express/resultext"
+)
 
 // MutexGuard protects the inner contents of a Mutex for safety and unlocking.
 type MutexGuard[T any, M interface{ Unlock() }] struct {
@@ -37,17 +41,6 @@ func (m Mutex[T]) Lock() MutexGuard[T, *sync.Mutex] {
 		m: m.m,
 		T: m.value,
 	}
-}
-
-// Unlock unlocks the Mutex accepting a value to set as the new or mutated value.
-// It is optional to pass a new value to be set but NOT required for there reasons:
-// 1. If the internal value is already mutable then no need to set as changes apply as they happen.
-// 2. If there's a failure working with the locked value you may NOT want to set it, but still unlock.
-// 3. Supports locked values that are not mutable.
-//
-// It is a run-time error if the Mutex is not locked on entry to Unlock.
-func (m Mutex[T]) Unlock() {
-	m.m.Unlock()
 }
 
 // PerformMut safely locks and unlocks the Mutex values and performs the provided function returning its error if one
