eaf46d717e3b978d942dbdb65cf6429d10a2920a
pchchv <ipchchv@gmail.com>
2025-07-02 20:12:54 +0300

    Add typed option functions for testStruct in option_test.go

--- optionext/option_test.go
+++ optionext/option_test.go
@@ -1,3 +1,11 @@
 package optionext
 
 type testStruct struct{}
+
+func returnTypedNoneOption() Option[testStruct] {
+	return None[testStruct]()
+}
+
+func returnTypedSomeOption() Option[testStruct] {
+	return Some(testStruct{})
+}
