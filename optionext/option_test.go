cbda64c7b5cb2ef51ead71aa32f374bbba0508f1
pchchv <ipchchv@gmail.com>
2025-07-02 20:13:25 +0300

    Add pointer option functions for testStruct in option_test.go

--- optionext/option_test.go
+++ optionext/option_test.go
@@ -9,3 +9,11 @@ func returnTypedNoneOption() Option[testStruct] {
 func returnTypedSomeOption() Option[testStruct] {
 	return Some(testStruct{})
 }
+
+func returnTypedNoneOptionPtr() Option[*testStruct] {
+	return None[*testStruct]()
+}
+
+func returnTypedSomeOptionPtr() Option[*testStruct] {
+	return Some(new(testStruct))
+}
