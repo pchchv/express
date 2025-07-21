cad71b2e823b4c3757834f3980605f207d0a8663
pchchv <ipchchv@gmail.com>
2025-07-21 20:42:48 +0300

    Add Token type

--- lexer.go
+++ lexer.go
@@ -3,3 +3,10 @@ package express
 
 // TokenKind is the type of token lexed.
 type TokenKind uint8
+
+// Token represents a lexed token.
+type Token struct {
+	Start uint32
+	Len   uint16
+	Kind  TokenKind
+}
