535bf7b1c524bb4d0f5b72dc4fef0c38dca4df0b
pchchv <ipchchv@gmail.com>
2025-07-21 20:55:08 +0300

    Add ErrInvalidSelectorPath error type with custom error message

--- errors.go
+++ errors.go
@@ -10,3 +10,12 @@ type ErrUnterminatedString struct {
 func (e ErrUnterminatedString) Error() string {
 	return fmt.Sprintf("Unterminated string `%s`", e.s)
 }
+
+// ErrInvalidSelectorPath represents an invalid selector string.
+type ErrInvalidSelectorPath struct {
+	s string
+}
+
+func (e ErrInvalidSelectorPath) Error() string {
+	return fmt.Sprintf("Invalid selector path `%s`", e.s)
+}
