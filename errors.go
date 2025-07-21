64918d131e4fdfadd8726ff3c1c6d32a63414883
pchchv <ipchchv@gmail.com>
2025-07-21 21:00:07 +0300

    Add ErrInvalidKeyword error type with custom error message

--- errors.go
+++ errors.go
@@ -19,3 +19,12 @@ type ErrInvalidSelectorPath struct {
 func (e ErrInvalidSelectorPath) Error() string {
 	return fmt.Sprintf("Invalid selector path `%s`", e.s)
 }
+
+// ErrInvalidKeyword represents an invalid keyword keyword.
+type ErrInvalidKeyword struct {
+	s string
+}
+
+func (e ErrInvalidKeyword) Error() string {
+	return fmt.Sprintf("Invalid keyword `%s`", e.s)
+}
