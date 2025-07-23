5ea1ff8563ee7d6d954c65e38a3fb69be047defd
pchchv <ipchchv@gmail.com>
2025-07-23 20:01:35 +0300

    Implement Calculate method for 'between' type to evaluate range comparisons

--- parser.go
+++ parser.go
@@ -1,6 +1,10 @@
 package express
 
 import (
+	"fmt"
+	"reflect"
+	"time"
+
 	"github.com/pchchv/extender/resultext"
 	"github.com/pchchv/goitertools"
 )
@@ -24,4 +28,42 @@ type between struct {
 	left  Expression
 	right Expression
 	value Expression
+}
+
+func (b between) Calculate(src []byte) (any, error) {
+	left, err := b.left.Calculate(src)
+	if err != nil {
+		return nil, err
+	}
+
+	right, err := b.right.Calculate(src)
+	if err != nil {
+		return nil, err
+	}
+
+	value, err := b.value.Calculate(src)
+	if err != nil {
+		return nil, err
+	}
+
+	// fast path, if any are nil/null there's no way to actually do the BETWEEN comparison
+	if left == nil || right == nil || value == nil {
+		return false, nil
+	}
+
+	leftType := reflect.TypeOf(left)
+	if !(leftType == reflect.TypeOf(right) && reflect.TypeOf(value) == leftType) {
+		return nil, ErrUnsupportedTypeComparison{s: fmt.Sprintf("%s < %s", left, right)}
+	}
+
+	switch v := value.(type) {
+	case string:
+		return v > left.(string) && v < right.(string), nil
+	case float64:
+		return v > left.(float64) && v < right.(float64), nil
+	case time.Time:
+		return v.After(left.(time.Time)) && v.Before(right.(time.Time)), nil
+	default:
+		return nil, ErrUnsupportedTypeComparison{s: fmt.Sprintf("%s < %s", left, right)}
+	}
 }
