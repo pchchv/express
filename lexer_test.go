0a6d398d429cf46f6097630a7359b6bc42494681
pchchv <ipchchv@gmail.com>
2025-07-23 19:38:54 +0300

    Implement collect func to tokenize input and handle errors in lexer_test

--- lexer_test.go
+++ lexer_test.go
@@ -1,2 +1,22 @@
 package express
 
+
+// Collect tokenizes the input and returns tokens or error lexing them.
+func collect(src []byte) (tokens []Token, err error) {
+	tokenizer := NewTokenizer(src)
+	for {
+		next := tokenizer.Next()
+		if next.IsNone() {
+			break
+		}
+
+		result := next.Unwrap()
+		if result.IsErr() {
+			return nil, result.Err()
+		}
+
+		tokens = append(tokens, result.Unwrap())
+	}
+
+	return
+}
