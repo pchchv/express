862ed0831338f92ab74f80a7d8b413c2cee8ec49
pchchv <ipchchv@gmail.com>
2025-07-23 19:43:08 +0300

    Add Expression type

--- parser.go
+++ parser.go
@@ -1,2 +1,11 @@
 package express
 
+
+// Expression Represents a stateless parsed expression that can be applied to JSON data.
+type Expression interface {
+	// Calculate executes the parsed expression and apply it against the supplied data.
+	//
+	// Will return `Err` if the expression cannot be applied to the
+	// supplied data due to invalid data type comparisons.
+	Calculate(src []byte) (any, error)
+}
