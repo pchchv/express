2538b9be7dfeed619f237c4a817462a2d55a945f
pchchv <ipchchv@gmail.com>
2025-07-23 19:56:25 +0300

    Add Parser type to handle expression parsing and tokenization

--- parser.go
+++ parser.go
@@ -1,5 +1,9 @@
 package express
 
+import (
+	"github.com/pchchv/extender/resultext"
+	"github.com/pchchv/goitertools"
+)
 
 // Expression Represents a stateless parsed expression that can be applied to JSON data.
 type Expression interface {
@@ -8,4 +12,10 @@ type Expression interface {
 	// Will return `Err` if the expression cannot be applied to the
 	// supplied data due to invalid data type comparisons.
 	Calculate(src []byte) (any, error)
+}
+
+// Parser parses and returns a supplied expression.
+type Parser struct {
+	Exp       []byte
+	Tokenizer goitertools.PeekableIterator[resultext.Result[Token, error]]
 }
