27bfefeebc6ea38ba2cfce1b5bc38b566a5a8db7
pchchv <ipchchv@gmail.com>
2025-07-25 19:10:45 +0300

    Implement Calculate method for containsAll type

--- parser.go
+++ parser.go
@@ -27,6 +27,7 @@ var (
 	_ Expression = (*endsWith)(nil)
 	_ Expression = (*contains)(nil)
 	_ Expression = (*startsWith)(nil)
+	_ Expression = (*containsAll)(nil)
 	_ Expression = (*containsAny)(nil)
 )
 
@@ -628,4 +629,70 @@ func (c containsAny) Calculate(src []byte) (any, error) {
 type containsAll struct {
 	left  Expression
 	right Expression
+}
+
+func (c containsAll) Calculate(src []byte) (any, error) {
+	left, err := c.left.Calculate(src)
+	if err != nil {
+		return nil, err
+	}
+
+	right, err := c.right.Calculate(src)
+	if err != nil {
+		return nil, err
+	}
+
+	switch l := left.(type) {
+	case string:
+		switch r := right.(type) {
+		case string:
+		OUTER1:
+			for _, c := range r {
+				for _, c2 := range l {
+					if c == c2 {
+						continue OUTER1
+					}
+				}
+				return false, nil
+			}
+		case []any:
+			for _, v := range r {
+				s, ok := v.(string)
+				if !ok || !strings.Contains(l, s) {
+					return false, nil
+				}
+			}
+			return true, nil
+		default:
+			return nil, ErrUnsupportedTypeComparison{s: fmt.Sprintf("%s CONTAINS_ALL %s", left, right)}
+		}
+	case []any:
+		switch r := right.(type) {
+		case []any:
+		OUTER3:
+			for _, rv := range r {
+				for _, lv := range l {
+					if reflect.DeepEqual(rv, lv) {
+						continue OUTER3
+					}
+				}
+				return false, nil
+			}
+		case string:
+		OUTER4:
+			for _, c := range r {
+				for _, v := range l {
+					if reflect.DeepEqual(string(c), v) {
+						continue OUTER4
+					}
+				}
+				return false, nil
+			}
+		default:
+			return nil, ErrUnsupportedTypeComparison{s: fmt.Sprintf("%s CONTAINS_ALL %s", left, right)}
+		}
+	default:
+		return nil, ErrUnsupportedTypeComparison{s: fmt.Sprintf("%s CONTAINS_ALL %s !", left, right)}
+	}
+	return true, nil
 }
