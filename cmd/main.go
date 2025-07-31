2e81c6e090ea249887c32edd49c9c5a258f76b02
pchchv <ipchchv@gmail.com>
2025-07-31 21:38:51 +0300

    Implement isInputFromPipe function

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func usage() {
 	fmt.Println("express [OPTIONS] <EXPRESSION> [DATA]")
 	flag.PrintDefaults()
+}
+
+func isInputFromPipe() bool {
+	fileInfo, _ := os.Stdin.Stat()
+	return fileInfo.Mode()&os.ModeCharDevice == 0
 }
 
 func main() {
