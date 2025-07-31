235b1fdf256bb19ae3660f59eceac270c0a98468
pchchv <ipchchv@gmail.com>
2025-07-31 21:31:50 +0300

    Add parsing benchmarks

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -2,6 +2,50 @@ package express
 
 import "testing"
 
+func BenchmarkParsingNumPlusNum(b *testing.B) {
+	benchParsing(b, "1 + 1")
+}
+
+func BenchmarkParsingIdentNum(b *testing.B) {
+	benchParsing(b, ".field1 + 1")
+}
+
+func BenchmarkParsingIdentIdent(b *testing.B) {
+	benchParsing(b, ".field1 + .field2")
+}
+
+func BenchmarkParsingFNameLName(b *testing.B) {
+	benchParsing(b, `.first_name + " " + .last_name`)
+}
+
+func BenchmarkParsingParenDiv(b *testing.B) {
+	benchParsing(b, `(1 + 1) / 2`)
+}
+
+func BenchmarkParsingParenDivIdents(b *testing.B) {
+	benchParsing(b, `(.field1 + .field2) / .field3`)
+}
+
+func BenchmarkParsingCompanyEmployees(b *testing.B) {
+	benchParsing(b, `.properties.employees > 20`)
+}
+
+func BenchmarkParsingParenNot(b *testing.B) {
+	benchParsing(b, `!(.f1 != .f2)`)
+}
+
+func BenchmarkParsingCoerceDateTimeSelector(b *testing.B) {
+	benchParsing(b, `COERCE .dt1 _datetime_ == COERCE .dt2 _datetime_`)
+}
+
+func BenchmarkParsingCoerceDateTimeSelectorMixed(b *testing.B) {
+	benchParsing(b, `COERCE .dt1 _datetime_ == COERCE "2022-01-02" _datetime_`)
+}
+
+func BenchmarkParsingCoerceDateTimeSelectorConstant(b *testing.B) {
+	benchParsing(b, `COERCE "2022-01-02" _datetime_ == COERCE "2022-01-02" _datetime_`)
+}
+
 func benchParsing(b *testing.B, expression string) {
 	b.SetBytes(int64(len(expression)))
 	for i := 0; i < b.N; i++ {
