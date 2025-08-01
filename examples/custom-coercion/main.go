5290cb5bdcb825160b47839277c349efae31ec2a
pchchv <ipchchv@gmail.com>
2025-08-01 11:12:22 +0300

    Implement a Calculate method for Star type

--- examples/custom-coercion/main.go
+++ examples/custom-coercion/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/pchchv/express"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pchchv/express"
+)
 
 type Star struct {
 	expression express.Expression
+}
+
+func (s *Star) Calculate(json []byte) (interface{}, error) {
+	inner, err := s.expression.Calculate(json)
+	if err != nil {
+		return nil, err
+	}
+
+	switch t := inner.(type) {
+	case string:
+		return strings.Repeat("*", len(t)), nil
+	default:
+		return nil, fmt.Errorf("cannot star value %v", inner)
+	}
 }
