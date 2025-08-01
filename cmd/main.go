9fecbec212c8977d2788a72c07c7ffe30d7588be
pchchv <ipchchv@gmail.com>
2025-08-01 11:08:35 +0300

    Implement CLI entrypoint to evaluate expressions against JSON input

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/pchchv/express"
+	"github.com/pchchv/extender/bytesext"
 )
 
 func usage() {
@@ -17,5 +22,98 @@ func isInputFromPipe() bool {
 }
 
 func main() {
+	var outputOriginal bool
+	flag.BoolVar(&outputOriginal, "o", false, "Indicates if the original data will be output after applying the expression. The results of the expression MUST be a boolean otherwise the output will be ignored.")
+	flag.Usage = usage
+	flag.Parse()
 
+	isPipe := isInputFromPipe()
+	if (flag.NArg() < 2 && !isPipe) || (flag.NArg() < 1 && isPipe) {
+		flag.Usage()
+		return
+	}
+
+	ex, err := express.Parse([]byte(flag.Arg(0)))
+	if err != nil {
+		flag.Usage()
+		return
+	}
+
+	var input []byte
+	w := bufio.NewWriter(os.Stdout)
+	if isPipe {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Buffer(make([]byte, 0, 200*bytesext.KiB), 5*bytesext.MiB)
+		if outputOriginal {
+			for scanner.Scan() {
+				input := scanner.Bytes()
+				result, err := ex.Calculate(input)
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "reading standard input:", err)
+					return
+				}
+
+				if result, ok := result.(bool); ok && result {
+					if _, err := w.Write(input); err != nil {
+						fmt.Fprintln(os.Stderr, "writing standard output:", err)
+					}
+
+					if err := w.WriteByte('\n'); err != nil {
+						fmt.Fprintln(os.Stderr, "writing standard output:", err)
+					}
+				}
+			}
+		} else {
+			enc := json.NewEncoder(w)
+			for scanner.Scan() {
+				result, err := ex.Calculate(scanner.Bytes())
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "reading standard input:", err)
+					return
+				}
+
+				if err := enc.Encode(result); err != nil {
+					fmt.Fprintln(os.Stderr, "encoding result to standard output:", err)
+					return
+				}
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+
+		if err = w.Flush(); err != nil {
+			fmt.Fprintln(os.Stderr, "writing standard output:", err)
+		}
+	} else {
+		input = []byte(flag.Arg(1))
+		result, err := ex.Calculate(input)
+		if err != nil {
+			flag.Usage()
+			return
+		}
+
+		if outputOriginal {
+			if result, ok := result.(bool); ok && result {
+				if _, err := w.Write(input); err != nil {
+					fmt.Fprintln(os.Stderr, "writing standard output:", err)
+				}
+
+				if err := w.WriteByte('\n'); err != nil {
+					fmt.Fprintln(os.Stderr, "writing standard output:", err)
+				}
+			}
+		} else {
+			enc := json.NewEncoder(w)
+			if err := enc.Encode(result); err != nil {
+				fmt.Fprintln(os.Stderr, "encoding result to standard output:", err)
+				return
+			}
+		}
+
+		if err = w.Flush(); err != nil {
+			fmt.Fprintln(os.Stderr, "writing standard output:", err)
+		}
+	}
 }
