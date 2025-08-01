b916e0696aa849068fa1ab77d5c1f93f32c49c02
pchchv <ipchchv@gmail.com>
2025-08-01 11:14:05 +0300

    Implement example demonstrating custom coercion with _star_ operator

--- examples/custom-coercion/main.go
+++ examples/custom-coercion/main.go
@@ -24,3 +24,27 @@ func (s *Star) Calculate(json []byte) (interface{}, error) {
 		return nil, fmt.Errorf("cannot star value %v", inner)
 	}
 }
+
+func main() {
+	// add custom coercion to the parser
+	// coercions start and end with an _(underscore)
+	guard := express.Coercions.Lock()
+	guard.T["_star_"] = func(_ *express.Parser, constEligible bool, expression express.Expression) (stillConstEligible bool, e express.Expression, err error) {
+		return constEligible, &Star{expression}, nil
+	}
+	guard.Unlock()
+
+	expression := []byte(`COERCE "My Name" _star_`)
+	input := []byte(`{}`)
+	ex, err := express.Parse(expression)
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := ex.Calculate(input)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v\n", result)
+}
